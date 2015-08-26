d2d5d5c4b1db21a45953448237f1a918235eefb5
Alfin Akhret <alfin@qasico.com>
2015-08-26 22:44:39 +0700

    add simple api server to the backend

--- app/backend/api.py
+++ app/backend/api.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3.4
 
 from flask import Flask
+from flask import send_from_directory
 from flask_restful import Resource, Api
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../frontend")
 api = Api(app)
 
-class HelloWorld(Resource):
-	def get(self):
-		return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
+@app.route('/')
+def index():
+	return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
